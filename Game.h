f62e7f6dbba5623056c571644b90aabd0c09c89d
20181220 이성수 <83389425+Holywater-lee@users.noreply.github.com>
2021-10-29 02:44:21 +0000

    12. 유일객체 Game

--- Game.h
+++ Game.h
@@ -9,7 +9,11 @@
 class Game
 {
 public:
-  Game() {}
+  static Game* Instance()
+  {
+    if (s_pInstance == 0) s_pInstance = new Game();
+    return s_pInstance;
+  }
   ~Game() {}
 
   bool init(const char* title, int xpos, int ypos, int height, int width, int flags);
@@ -18,13 +22,17 @@ public:
   bool running();
   void handleEvents();
   void clean();
+  SDL_Renderer* getRenderer() const { return m_pRenderer; }
 
 private:
+  Game() {}
+  static Game* s_pInstance;
   SDL_Window* m_pWindow;
   SDL_Renderer* m_pRenderer;
   bool m_bRunning;
 
   std::vector<GameObject*> m_gameObjects;
 };
+typedef Game TheGame;
 
 #endif
