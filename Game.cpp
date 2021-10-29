1438c322f41e1212ca413cfe835f0e5955546c47
20181220 이성수 <83389425+Holywater-lee@users.noreply.github.com>
2021-10-29 02:12:37 +0000

    6. 애니메이션 스프라이트

--- Game.cpp
+++ Game.cpp
@@ -21,13 +21,14 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
     return false;
   }
 
-  SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+  SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
   m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
   SDL_FreeSurface(pTempSurface);
 
-  SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
   m_sourceRectangle.x = 0;
   m_sourceRectangle.y = 0;
+  m_sourceRectangle.w = 128;
+  m_sourceRectangle.h = 82;
 
   m_destinationRectangle.w = m_sourceRectangle.w;
   m_destinationRectangle.h = m_sourceRectangle.h;
@@ -51,7 +52,7 @@ void Game::render()
 
 void Game::update()
 {
-
+  m_sourceRectangle.x = 128 * ((SDL_GetTicks() / 100) % 6);
 }
 
 bool Game::running()
